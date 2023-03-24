181afa47519ec6db264eb18bdc739c8c9ee0b7ca
Co <62308340+Cohen-Koen@users.noreply.github.com>
2023-03-24 19:22:05 +0000

    added ask functions

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,35 @@ module Main where
 
 factorial :: Int -> Int
 factorial x = if x == 0 then 1 else x*factorial (x-1)
+gammaFunction :: Int -> Double
+gammaFunction x = if x == 0 then 1 else (fromIntegral x) * gammaFunction (x-1)
+translateToBinary :: Int -> [Int]
+translateToBinary x = if x == 0 then [] else (translateToBinary (x `div` 2)) ++ [x `mod` 2]
 
-
-
+askFact :: IO ()
 askFact = do
     putStrLn "n! where n =:"
     n <- getLine
     let n' = read n :: Int -- this line fixes the whole thing...
-    putStrLn (show (factorial n'))   
+    putStrLn ("The factorial of " ++ n ++ " is: " ++ show (factorial n'))   
 
+askTrans :: IO ()
+askTrans = do
+    putStrLn "Enter a number to translate to binary:"
+    n <- getLine
+    let n' = read n :: Int
+    putStrLn ("The binary representation of " ++ n ++ " is: " ++ show (translateToBinary n'))
 
+askBinary :: IO ()
+askGamma = do
+    putStrLn "Enter a number to calculate the gamma function:"
+    n <- getLine
+    let n' = read n :: Int
+    putStrLn ("The gamma function of " ++ n ++ " is: " ++ show (gammaFunction n'))
 
 main :: IO ()
 main = do
     askFact
+    askTrans
+    askGamma
+    askBinary
--- readme.md
+++ readme.md
@@ -1,13 +1,17 @@
-# How to setup:
+# How to setup
+
 Run the following in the terminal
 
-```
+```bash
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 ```
-> this code will setup haskell <br/>
+
+## this code will setup haskell \
+
 > ---
-> `ghcup tui` will let you open a terminal interface <br/>
-> `cabal build` will build the project <br/>
-> `cabal init --interactive` should let you overwrite and create your own project <br/>
+>
+> `ghcup tui` will let you open a terminal interface \
+> `cabal build` will build the project \
+> `cabal init --interactive` should let you overwrite and create your own project \
 > `cabal run` will run the executable
-> 
+>
